4512cdb02122f3b7dfade070d1b3177ba01d9f74
Seantay <risingcinders@gmail.com>
2020-10-12 11:57:56 -0500

    With Validations

--- tvshowapp/models.py
+++ tvshowapp/models.py
@@ -1,6 +1,32 @@
 from django.db import models
 import datetime
 
+
+class ShowManager(models.Manager):
+    def basic_validator(self,postData):
+        errors={}
+        if len(postData['title']) < 2:
+            errors["title"] = "Title must be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors["network"] = "Network must be at least 3 characters"
+        if (len(postData['desc']) < 10) and (len(postData['desc']) != 0):
+            errors['desc'] = "Description must be at least 10 characters or left blank"
+        if (datetime.date.today() < datetime.datetime.strptime(postData['date'],"%Y-%m-%d").date()):
+            errors['date'] = "Date must be in the past"
+        shows = Show.objects.all()
+        titlelist = []
+        for show in shows:
+            titlelist.append(show.title)
+        # if (Show.objects.get(title = postData['title']) != None):
+        if postData['title'] in titlelist:
+            errors['duplicate'] = "That show already exists"
+        return errors
+        
+        #title 2 char,
+        # network 3 car
+        # desc 10 car
+
+
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
@@ -8,3 +34,4 @@ class Show(models.Model):
     desc = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- tvshowapp/views.py
+++ tvshowapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 from .models import Show
 import datetime
 
@@ -11,17 +12,20 @@ def listshows(request):
 
 
 def newshow(request):
-    if request.method == 'POST':
-        Show.objects.create(
-            title=request.POST['title'], network=request.POST['network'], release_date=request.POST['date'], desc=request.POST['desc'])
     return render(request, "shownew.html")
 
 
 def newshowprocess(request):
-    if request.method == 'POST':
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/TVShowLibrary/shows/new')
+    else:
         Show.objects.create(title=request.POST['title'], network=request.POST['network'],
-                            release_date=request.POST['date'], desc=request.POST['desc'])
-    return redirect('../shows')
+                        release_date=request.POST['date'], desc=request.POST['desc'])
+        newestshow = Show.objects.get(title = request.POST['title'])
+    return redirect('../shows/' + str(newestshow.id))
 
 
 def showdetail(request, showid):
@@ -33,21 +37,27 @@ def showdetail(request, showid):
 
 def showedit(request, showid):
     date = Show.objects.get(id=showid).release_date
-    date = datetime.date.strftime( date, "%Y-%m-%d")
+    date = datetime.date.strftime(date, "%Y-%m-%d")
     context = {
         "thisshow": Show.objects.get(id=showid),
-        "datesample" : date
+        "datesample": date
     }
     return render(request, "showedit.html", context)
 
 
 def showeditprocess(request, showid):
-    thisshow = Show.objects.get(id=showid)
-    thisshow.title = request.POST['title']
-    thisshow.network = request.POST['network']
-    thisshow.release_date = request.POST['date']
-    thisshow.desc = request.POST['desc']
-    thisshow.save()
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/TVShowLibrary/shows/' + str(showid) + '/edit')
+    else:
+        thisshow = Show.objects.get(id=showid)
+        thisshow.title = request.POST['title']
+        thisshow.network = request.POST['network']
+        thisshow.release_date = request.POST['date']
+        thisshow.desc = request.POST['desc']
+        thisshow.save()
     return redirect('../' + str(showid))
 
 
@@ -55,6 +65,7 @@ def showdelete(request, showid):
     thisshow = Show.objects.get(id=showid)
     thisshow.delete()
     return redirect('/TVShowLibrary/shows')
+
 
 def backtohome(request):
     return redirect('./shows')
